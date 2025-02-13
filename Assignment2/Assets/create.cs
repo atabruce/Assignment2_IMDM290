79dcdf74d3e530c64ccff4e727eea1bed92d0dea
atabruce <ariellebruce2004@gmail.com>
2025-02-13 15:51:37 -0500

    aaa

--- Assignment2/Assets/create.cs
+++ Assignment2/Assets/create.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Create : MonoBehaviour
+public class Animate : MonoBehaviour
 {
     GameObject[] spheres;
-    static int numSphere = 100;  // Number of spheres to create
+    static int numSphere = 100;
     float time = 0f;
     Vector3[] initPos;
 
@@ -17,21 +17,38 @@ public class Create : MonoBehaviour
 
         for (int i = 0; i < numSphere; i++)
         {
-            float t = (i / (float)numSphere) * 2 * Mathf.PI; // t values from 0 to 2π
-            float x = Mathf.Sqrt(2) * Mathf.Pow(Mathf.Sin(t), 3);
-            float y = -Mathf.Pow(Mathf.Cos(t), 3) - Mathf.Pow(Mathf.Cos(t), 2) + 2 * Mathf.Cos(t);
-            float z = 0f; // Keep it 2D in the XY plane
-
-            // Create the sphere
             spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            initPos[i] = new Vector3(x * 5f, y * 5f, z); // Scale the heart size
+
+            float t = Mathf.Lerp(0, 2 * Mathf.PI, i / (float)numSphere);
+            float x = 3f * Mathf.Pow(Mathf.Sin(t), 3);
+            float y = -2 * Mathf.Pow(Mathf.Cos(t), 3) - 2 * Mathf.Pow(Mathf.Cos(t), 2) + 4 * Mathf.Cos(t);
+            float z = 0;
+
+            initPos[i] = new Vector3(x, y, z);
             spheres[i].transform.position = initPos[i];
 
-            // Assign colors based on position
+            // Set initial color
             Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
-            float hue = (float)i / numSphere; // Different colors along the heart shape
-            Color color = Color.HSVToRGB(hue, 1f, 1f);
-            sphereRenderer.material.color = color;
+            sphereRenderer.material.color = Color.red;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        // Calculate scale factor using sine wave
+        float scaleFactor = 1f + 0.3f * Mathf.Sin(time * 2f); // oscillates between 0.7 and 1.3
+
+        foreach (GameObject sphere in spheres)
+        {
+            // Apply uniform scaling
+            sphere.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+
+            // Color pulse between red and pink
+            Color color = Color.Lerp(Color.red, new Color(1f, 0.75f, 0.8f), (Mathf.Sin(time * 2f) + 1f) / 2f);
+            sphere.GetComponent<Renderer>().material.color = color;
         }
     }
 }
